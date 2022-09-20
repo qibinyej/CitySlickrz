11192a34410671495c413ac436d7ea82673669cd
Ignacio Yz <ignacioyz@gmail.com>
2022-09-20 14:07:10 -0400

    some file structure, react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from './Login';
+import Navbar from './Navbar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,18 +13,20 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    
       <div className="App">
+        <Navbar>
         <Switch>
-          <Route path="/testing">
+          <Route path='/login' element={<Login/>} />
             <h1>Test Route</h1>
-          </Route>
+         
           <Route path="/">
             <h1>Page Count: {count}</h1>
           </Route>
         </Switch>
+        </Navbar>
       </div>
-    </BrowserRouter>
+ 
   );
 }
 
