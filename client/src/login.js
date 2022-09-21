d61c036c955783979449bd97f47a163c4077912e
Ignacio Yz <ignacioyz@gmail.com>
2022-09-21 12:38:49 -0400

    options for background

--- client/src/login.js
+++ client/src/login.js
@@ -8,8 +8,8 @@ function Login() {
 
 return (
     <div id="login-parent">
-        <video id="login-backgound-vid" autoPlay muted>
-                <source src="https://player.vimeo.com/external/638069754.sd.mp4?s=3a19a2ad6cf8dcd04a6e11322891031b79aefe06&profile_id=164&oauth2_token_id=57447761" type="video/mp4"> 
+        <video id="login-backgound-vid" loop autoPlay muted>
+                <source src="https://player.vimeo.com/external/370845105.sd.mp4?s=b9419e54cff17ff6aec0f50e194d3e4e92a06129&profile_id=164&oauth2_token_id=57447761" type="video/mp4"> 
                 </source>
         </video>
         
