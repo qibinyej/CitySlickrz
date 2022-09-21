e746833486ea671d3fe8d5a45585e9a69ece98c5
Ignacio Yz <ignacioyz@gmail.com>
2022-09-21 11:44:46 -0400

    login background vid

--- client/src/login.js
+++ client/src/login.js
@@ -7,8 +7,12 @@ function Login() {
 
 
 return (
-    <div>
-        <h1>Hello</h1>
+    <div id="login-parent">
+        <video id="login-backgound-vid" autoPlay muted>
+                <source src="https://player.vimeo.com/external/638069754.sd.mp4?s=3a19a2ad6cf8dcd04a6e11322891031b79aefe06&profile_id=164&oauth2_token_id=57447761" type="video/mp4"> 
+                </source>
+        </video>
+        
         </div>
 )
 
