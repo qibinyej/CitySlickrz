cf4045a0cd69f58a91b2322689a722875c86022d
Ignacio Yz <ignacioyz@gmail.com>
2022-09-20 21:41:29 -0400

    minor nav and route additions

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import './navbar.css';
 
 
- function Navbar() {
-    
-    
-    return (
-<ul id="nav-ul">
+function Navbar() {
+
+
+    return (<nav id="nav">
+        <ul id="nav-ul">
             <li id="nav-li"><Link to="/login">Home</Link></li>
 
             <li id="nav-li"><Link>route 1</Link></li>
@@ -14,6 +15,7 @@ import { Link } from "react-router-dom"
             <li id="nav-li"><Link>route 3</Link></li>
             <li id="nav-li"><Link>route 4</Link></li>
         </ul>
+    </nav>
     )
 
 }
