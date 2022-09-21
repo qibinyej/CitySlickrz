36ebc38edc5a0f9f7f982d7cb0bd63e63151a4af
Ignacio Yz <ignacioyz@gmail.com>
2022-09-20 20:53:08 -0400

    additional routes (sans components)

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -6,10 +6,13 @@ import { Link } from "react-router-dom"
     
     
     return (
-        <ul>
-            <li>
-            <Link to="/login">Home</Link>
-            </li>
+<ul id="nav-ul">
+            <li id="nav-li"><Link to="/login">Home</Link></li>
+
+            <li id="nav-li"><Link>route 1</Link></li>
+            <li id="nav-li"><Link>route 2</Link></li>
+            <li id="nav-li"><Link>route 3</Link></li>
+            <li id="nav-li"><Link>route 4</Link></li>
         </ul>
     )
 
