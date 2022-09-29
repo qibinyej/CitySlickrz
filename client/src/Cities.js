9afa2a5eee145eb5a2df39699be0183db8409b99
Ignacio Yz <ignacioyz@gmail.com>
2022-09-29 15:56:51 -0400

    route deliniations

--- client/src/Cities.js
+++ client/src/Cities.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import './cities.css';
 
 function Cities(){
 
 
     return(
-    <></>
+    <div id="parent-div">
+        <h2 id="basic-data">Will Render our cities</h2>
+    </div>
     )
 }
 
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,9 +1,12 @@
 import React from "react";
+import './profile.css';
 
 function Profile() {
 
 return(
-    <></>
+    <div id='profile-parent-div'>
+        <h2 id="basic-data">Will Render User Info</h2>
+    </div>
 )
 }
 
