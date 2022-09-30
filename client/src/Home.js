654d61d35e8b5e1f3e4fc8e6a8456f44207a30bd
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-09-30 00:23:44 -0400

    12 am changes pt 2

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,17 +7,15 @@ function Home(){
 return (
 <div className="home-page">
     <div id="welcome">
-    
-       <h1>
+        <h1>
         Welcome City Slickr!
         </h1>
         
-       
+    
     </div>
     <img id="home-photo" src="https://photostorage.explorest.com/usa/hawaii/vlim-mai-poina-palmtree-feed.jpg"></img>
-    
+    {/* Possible Homepage top 10 US cities to live in. Have a hover, bounce effect, etc. */}
     <div className="city-container">
-        
         <div class="city1">City 1</div>
         <div class="city2">City 2</div>
         <div class="city3">City 3</div>
@@ -28,7 +26,6 @@ return (
         <div class="city8">City 8</div>
         <div class="city9">City 9</div>
         <div class="city10">City 10</div>
-
     </div>
 </div>
 )
