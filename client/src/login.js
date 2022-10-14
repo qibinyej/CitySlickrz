791eb9349046473ad8143cce667cf3fa577d6cf2
Ignacio Yz <ignacioyz@gmail.com>
2022-10-14 18:05:15 -0400

    mo modal

--- client/src/login.js
+++ client/src/login.js
@@ -7,20 +7,20 @@ import Signup from "./signup";
 function Login() {
 
 
-        const [signUp, setSignUp] = useState(false)
+        const [openSignUp, setOpenSignUp] = useState(false)
 
 
 
 
         return (
                 <div id="login-parent">
-                        {signUp && <Signup signUp={signUp} closeSignUp ={setSignUp}/>}
+                        {openSignUp && <Signup signUp={ openSignUp } closeSignUp ={ setOpenSignUp }/>}
                         <video id="login-backgound-vid" loop autoPlay muted>
                                 <source src="https://player.vimeo.com/progressive_redirect/playback/712741455/rendition/360p/file.mp4?loc=external&oauth2_token_id=57447761&signature=1d32f61a841a2f36d5d2524755d5e38b4ca2c2964089ca5edeeb5d4db1b062a8" type="video/mp4"></source>
                         </video>
                         <div id="login-credentials-div">
                                 <h1 id="siteTitle">CitySlickrz</h1>
-                                <h3 id="header">Don't have a profile? <strong onClick={() => {setSignUp(true)}} style={{ color: 'blue', cursor: 'pointer' }}>Sign Up</strong> </h3>
+                                <h3 id="header">Don't have a profile? <strong onClick={() => {setOpenSignUp(true)}} style={{ color: 'blue', cursor: 'pointer' }}>Sign Up</strong> </h3>
                                 <form>
                                         <input required id="input" placeholder="Username"></input>
                                         <input required id="input" placeholder="Password" type="password"></input>
--- client/src/signup.js
+++ client/src/signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 // import { useNavigate } from 'react-router-dom';
 
 
-function Signup() {
+function Signup( {openSignUp, closeSignUp }) {
 
   
 
